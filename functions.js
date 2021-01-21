30ce70d1b05a7da153e390525053febd1a017f86
Patrick Navarre <patrick.navarre@codeimmersives.com>
2021-01-21 10:20:22 -0500

    added-some-frail-notes

--- functions.js
+++ functions.js
@@ -1,22 +1,32 @@
+// returning whatever value is being passed in as a parameter
+
 const identity = function (value) {
   return value;
 };
 
+
+
 const first = function (array, n) {
+  // create an empty array
   let result = []; 
-  if(n === undefined)
-  return array[0];
+  // if undefined then return the first element in the array
+  if(n === undefined){
+    return array[0];
+  }
+  // if the index is greater than n push into the array the index
   for (let i = 0; i < n; i++) {
     if(i < array.length)
     result.push(array[i]);
   } 
+  // return that array
   return result;
 }
 
 const last = function(array, n) {
   let result = [];
-  if(n === undefined)
+  if(n === undefined){
     return array.length;
+  }
     for (let i = 0; i < n; i++){
       if(i < array.length){
         result.push(array[i]);
@@ -25,7 +35,12 @@ const last = function(array, n) {
     return result;
   }
 
+// const last = function(array, n) {
+//   return array.slice(-1, n).join(' ');
+// }
 
+
+// executes the callback function "iterator" on each element in the array
 const each = function (collection, iterator) {
   return collection.forEach(iterator)
 };
